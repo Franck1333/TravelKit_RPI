74bc69b45e93e296e48e7b56aeac1ed67538fee7
Franck1333 <fun13@live.fr>
2018-08-15 02:22:18 +0200

    Adding Setup Files :ballot_box_with_check:
    Hello everyone ,
    Tonight I'm adding new files for setup the project with everything that the project require to work properly.
    
    That's my first Setup.py python file , im testing it ^^.
    
    If it's doesn't work as expected , just follow the current Readme.md available on the Main page of the project on Github.com.
    
    Thank You ,  Francky :+1:

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+#Aides : Using set-me-up.py from https://gist.github.com/ojii/3906682 ,
+#Aides : The generator "set-me-up.py" need a "__init__.py" file in the main root directorie to work .
+
+from setuptools import setup, find_packages
+
+setup(
+    name='TravelKit_RPI',
+    version="1.13",
+    author='Franck Rochat',
+    author_email='rochat.franck@gmail.com',
+    description='This Project give you useful details about the environement where you are by Using an USB GPS and some API with an Internet Connection',
+    url='https://github.com/Franck1333/TravelKit_RPI',
+    license='lgpl',
+    packages=find_packages(),
+    include_package_data=False,
+    install_requires=["fcntl","dot3k","sys","time","requests","pyowm","googlemaps"], #Get the Dependencies from Pypi (pip install)
+    dependency_links=['https://github.com/pimoroni/displayotron'], #Get the Dependencies via HTTP(s)
+)
